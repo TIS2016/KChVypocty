06dd5be77cecb2088f6adb13ff3bc26de995ab50
Katarina Fabianova <kfabianova11@gmail.com>
2016-12-13 00:21:33 +0100

    pridane stlpce

--- app/database_entities/Calculation.php
+++ app/database_entities/Calculation.php
@@ -1,14 +1,14 @@
 <?php
 
-
+namespace App\Db;
 /**
  * @Entity @Table(name="calculations")
  *
  **/
-class Calculation
-{
+class Calculation {
+
     /** @Id @Column(type="integer") @GeneratedValue **/
-    protected $productId;
+    protected $calculationID;
 
     /** @Column(type="string") **/
     protected $jobType;
@@ -25,7 +25,7 @@ class Calculation
     /** @Column(type="string") **/
     protected $user;
 
-    /** @Column(type="date") **/
+    /** @Column(type="string") **/
     protected $date;
 
     /** @Column(type="string") **/
@@ -34,16 +34,256 @@ class Calculation
     /** @Column(type="string") **/
     protected $path;
 
+    /** @Column(type="string") **/
+    protected $infoInput;
+
+    /** @Column(type="string") **/
+    protected $infoEnd;
+
+    /** @Column(type="string") **/
+    protected $energy;
+
+    /** @Column(type="string") **/
+    protected $thermoChemistry;
+
 
     /**
-     * @OneToMany(targetEntity="Coordinate", mappedBy="calculation")
+     * @return mixed
+     */
+    public function getCalculationID()
+    {
+        return $this->calculationID;
+    }
+
+    /**
+     * @param mixed $calculationID
+     */
+    public function setCalculationID($calculationID)
+    {
+        $this->calculationID = $calculationID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getJobType()
+    {
+        return $this->jobType;
+    }
+
+    /**
+     * @param mixed $jobType
+     */
+    public function setJobType($jobType)
+    {
+        $this->jobType = $jobType;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @param mixed $method
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBasisSet()
+    {
+        return $this->basisSet;
+    }
+
+    /**
+     * @param mixed $basisSet
+     */
+    public function setBasisSet($basisSet)
+    {
+        $this->basisSet = $basisSet;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStechiometry()
+    {
+        return $this->stechiometry;
+    }
+
+    /**
+     * @param mixed $stechiometry
+     */
+    public function setStechiometry($stechiometry)
+    {
+        $this->stechiometry = $stechiometry;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getServer()
+    {
+        return $this->server;
+    }
+
+    /**
+     * @param mixed $server
+     */
+    public function setServer($server)
+    {
+        $this->server = $server;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param mixed $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCoordinates()
+    {
+        return $this->coordinates;
+    }
+
+    /**
+     * @param mixed $coordinates
+     */
+    public function setCoordinates($coordinates)
+    {
+        $this->coordinates = $coordinates;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getInfoInput()
+    {
+        return $this->infoInput;
+    }
+
+    /**
+     * @param mixed $infoInput
+     */
+    public function setInfoInput($infoInput)
+    {
+        $this->infoInput = $infoInput;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInfoEnd()
+    {
+        return $this->infoEnd;
+    }
+
+    /**
+     * @param mixed $infoEnd
+     */
+    public function setInfoEnd($infoEnd)
+    {
+        $this->infoEnd = $infoEnd;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEnergy()
+    {
+        return $this->energy;
+    }
+
+    /**
+     * @param mixed $energy
+     */
+    public function setEnergy($energy)
+    {
+        $this->energy = $energy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getThermoChemistry()
+    {
+        return $this->thermoChemistry;
+    }
+
+    /**
+     * @param mixed $thermoChemistry
+     */
+    public function setThermoChemistry($thermoChemistry)
+    {
+        $this->thermoChemistry = $thermoChemistry;
+    }
+
+
+    /**
+     * @OneToMany(targetEntity="Coordinate", mappedBy="calculation", cascade="persist")
      */
     private $coordinates;
+
 
 
     public function __construct(){
         $this->coordinates = new \Doctrine\Common\Collections\ArrayCollection();
     }
+
 
 
 }
